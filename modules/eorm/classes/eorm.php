a54fb11211953dc5af552ce7b27a70d3c1747eb3
Brandon R. Stoner <github@monokro.me>
2010-12-01 15:04:21 -0700

    Added EORM modules directory.

---
+++ modules/eorm/classes/eorm.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class EORM exends EORM_EORM
+{
+
+	// You can extend the extended ORM here.
+
+}
+
---
+++ modules/eorm/classes/eorm/eorm.php
@@ -0,0 +1,44 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class EORM_EORM exends ORM
+{
+	private $_column_list = array();
+
+	public function __construct()
+	{
+		$this->init_columns();
+		$this->_construct_columns();
+	}
+
+	protected function init_columns()
+	{
+	}
+
+	/**
+	 * Gets all data columns that this ORM field uses and converts them into
+	 * Kohana's standard ORM structure.
+	 */
+	private function _construct_columns()
+	{
+
+			// Loop through all attributes and add Field instances to the column list
+		for ($attribute in get_class_vars($this))
+		{
+			if ($attribute instanceof EORM_Field)
+				array_push($this->_column_list, $attribute);
+		}
+
+	}
+}
+
+/**
+ * Demo of using this ORM.
+ */
+class Model_User extends EORM
+{
+	public $nickname;
+	public $password;
+
+	protected function init_columns
+}
+
